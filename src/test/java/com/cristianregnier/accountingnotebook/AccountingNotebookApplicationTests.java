b11b4a4555cdf035db8ee237c9dafd81e563d70f
cristianregnier <cristianregnier@gmail.com>
2020-07-30 11:37:20 -0300

    App skeleton

---
+++ src/test/java/com/cristianregnier/accountingnotebook/AccountingNotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianregnier.accountingnotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingNotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
